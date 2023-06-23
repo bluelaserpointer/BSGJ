ebab248c06983797d886041119d8fb29b1957876
YoshinoNao <113161290+Yoshino-Nao@users.noreply.github.com>
2023-06-23 09:34:59 +0900

    scrapのspriteを追加、BrokenFloor.csのDebug.Logを削除

--- ProgrammerProducts/ThreeLives/Assets/Yoshino/Scripts/BrokenFloor.cs
+++ ProgrammerProducts/ThreeLives/Assets/Yoshino/Scripts/BrokenFloor.cs
@@ -7,7 +7,6 @@ public class BrokenFloor : MonoBehaviour
     [SerializeField] private float durability = 3;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.rigidbody.velocity.magnitude);
         if (collision.rigidbody.velocity.magnitude >= durability)   
         {
             Destroy(this.gameObject);
