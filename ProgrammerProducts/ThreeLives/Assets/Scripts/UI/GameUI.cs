d725710b3baf1e84830637bc502ecb2e3e4fff3d
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2023-07-07 14:45:42 +0800

    feat: item collect system and progress ui

--- ProgrammerProducts/ThreeLives/Assets/Scripts/UI/GameUI.cs
+++ ProgrammerProducts/ThreeLives/Assets/Scripts/UI/GameUI.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [DisallowMultipleComponent]
 public class GameUI : MonoBehaviour
 {
     [SerializeField]
     ItemBar _itemBar;
+    [SerializeField]
+    Text _itemCollectProgressText;
 
     public ItemBar ItemBar => _itemBar;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+    public void UpdateUI()
+    {
+        _itemCollectProgressText.text = SaveData.Instance.collectedItems.Count + "/3";
+    }
 }
