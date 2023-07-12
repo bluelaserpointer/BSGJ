2d74325f0a7a36973dc78c2c029fbc56c72528b9
YoshinoNao <113161290+Yoshino-Nao@users.noreply.github.com>
2023-07-12 13:28:23 +0900

    SE追加、ボタン画像入れ替え

--- ProgrammerProducts/ThreeLives/Assets/Yoshino/Scripts/BrokenFloor.cs
+++ ProgrammerProducts/ThreeLives/Assets/Yoshino/Scripts/BrokenFloor.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BrokenFloor : MonoBehaviour
 {
     [SerializeField] private float durability = 3;
+    [SerializeField] private AudioClip se;
+    [SerializeField] AudioSource source;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.tag == "Player")
+        {
+            source.PlayOneShot(se);
+        }
+
         if (collision.rigidbody.velocity.magnitude >= durability)   
         {
             Destroy(this.gameObject);
